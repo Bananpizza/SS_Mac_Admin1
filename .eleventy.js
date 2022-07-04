8143bad3d7a8541942e9b4975e3d547c1a58b16c
Bananpizza <niklas.bruhn@gmail.com>
2022-07-04 09:06:25 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
-// const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const syntaxHighlight = require("eleventy-md-syntax-highlight")
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
@@ -19,12 +18,7 @@ module.exports = function (eleventyConfig) {
   });
 
   // Syntax Highlighting for Code blocks
-  // eleventyConfig.addPlugin(syntaxHighlight);
-
-  // Syntax Markdown Highlight Line Numbers
-  eleventyConfig.addPlugin(syntaxHighlight, { 
-    showLineNumbers: false
-  });
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   // To Support .yaml Extension in _data
   // You may remove this if you can use JSON
